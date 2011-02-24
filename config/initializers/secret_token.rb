5259bc351ddccaa8956dddf4fb283ac4d2b94c58
Takeshi Sato <takeshi-sato@netyear.net>
2011-02-24 11:55:43 +0900

    プロジェクトを作成。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jqmoblog::Application.config.secret_token = '878ac2f05db0b637f1817ee0b026ec0019c5c9962ebbddbedb45ede5d82d1fa6e4d5768581b1fda28d15b968e76a81c0c030ad9cb251c9b2441651aa925ceac9'
